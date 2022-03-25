4e9f0e889f5f20f49432757cbefd7bb5e248f85e
zfluok <zfluok@isoftstone.com>
2022-03-25 17:57:06 +0800

    111

---
+++ SmartCode/SmartCode.Framework/Exporter/OracleExporter.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SmartCode.Framework.PhysicalDataModel;
+using SqlSugar;
+
+namespace SmartCode.Framework.Exporter
+{
+    public class OracleExporter : Exporter, IExporter
+    {
+        public OracleExporter(string connectionString) : base(connectionString)
+        {
+
+        }
+
+        /// <summary>
+        /// 初始化
+        /// </summary>
+        /// <returns></returns>
+        public override Model Init()
+        {
+            var model = new Model { Database = "MySql" };
+            try
+            {
+                //model.Tables = this.GetTables();
+                //model.Views = this.GetViews();
+                //model.Procedures = this.GetProcedures();
+                return model;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public override List<DataBase> GetDatabases()
+        {
+            #region MyRegion
+            var dbMaintenance = SugarFactory.GetDbMaintenance(DbType.Oracle, DbConnectString);
+            var dbClient = SugarFactory.GetInstance(DbType.Oracle, DbConnectString);
+            var dataBaseList = dbMaintenance.GetDataBaseList(dbClient);
+            var list = new List<DataBase>();
+            dataBaseList.ForEach(dbName =>
+            {
+                var dBase = new DataBase
+                {
+                    DbName = dbName,
+                    IsSelected = false
+                };
+                list.Add(dBase);
+            });
+            return list;
+            #endregion
+        }
+
+        /// <summary>
+        /// 根据对象ID获取列信息
+        /// </summary>
+        /// <param name="objectId"></param>
+        /// <returns></returns>
+        public override Columns GetColumnInfoById(string objectId)
+        {
+            var columns = new Columns(500);
+            var dbMaintenance = SugarFactory.GetDbMaintenance(DbType.Oracle, DbConnectString);
+            var viewList = dbMaintenance.GetColumnInfosByTableName(objectId);
+            viewList.ForEach(v =>
+            {
+                if (columns.ContainsKey(v.DbColumnName))
+                {
+                    return;
+                }
+                var column = new Column(v.DbColumnName, v.DbColumnName, v.DbColumnName, v.DataType, v.ColumnDescription);
+                column.Length = "";
+                switch (v.DataType)
+                {
+                    case "char":
+                    case "nchar":
+                    case "time":
+                    case "text":
+                    case "binary":
+                    case "varchar":
+                    case "nvarchar":
+                    case "varbinary":
+                    case "datetime2":
+                    case "datetimeoffset":
+                        column.Length = $"({v.Length})"; break;
+                    case "numeric":
+                    case "decimal":
+                        column.Length = $"({v.Length},{v.Scale})"; break;
+                }
+
+                column.ObjectId = objectId.ToString();
+                column.ObjectName = v.DbColumnName;
+                column.IsIdentity = v.IsIdentity;
+                column.IsNullable = v.IsNullable;
+                column.DefaultValue = !string.IsNullOrEmpty(v.DefaultValue) && v.DefaultValue.Contains("((") ? v.DefaultValue.Replace("((", "").Replace("))", "") : v.DefaultValue;
+                column.DataType = v.DataType;
+                column.OriginalName = v.DbColumnName;
+                column.Comment = v.ColumnDescription;
+                column.IsPrimaryKey = v.IsPrimarykey;
+                columns.Add(v.DbColumnName, column);
+            });
+            return columns;
+        }
+
+        public override string GetScriptInfoById(string objectId, DbObjectType objectType)
+        {
+            var dbMaintenance = SugarFactory.GetDbMaintenance(DbType.Oracle, DbConnectString);
+            var scriptInfo = dbMaintenance.GetScriptInfo(objectId, objectType);
+            return scriptInfo.Definition;
+        }
+
+        public override string CreateTableSql(string tableName, List<Column> columns)
+        {
+            return "";
+        }
+
+        public override string SelectSql(string tableName, List<Column> columns)
+        {
+            return "";
+        }
+
+        public override string InsertSql(string tableName, List<Column> columns)
+        {
+            return "";
+        }
+
+        public override string UpdateSql(string tableName, List<Column> columns)
+        {
+            return "";
+        }
+
+        public override string DeleteSql(string tableName, List<Column> columns)
+        {
+            return "";
+        }
+
+        public override string AddColumnSql(string tableName, List<Column> columns)
+        {
+            return "";
+        }
+
+        public override string AlterColumnSql(string tableName, List<Column> columns)
+        {
+            return "";
+        }
+
+        public override string DropColumnSql(string tableName, List<Column> columns)
+        {
+            return "";
+        }
+    }
+}
