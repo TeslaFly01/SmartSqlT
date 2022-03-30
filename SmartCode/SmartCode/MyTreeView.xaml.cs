3d7e18b306a73fda310ac184df84f94d86a953cc
zfluok <zfluok@isoftstone.com>
2022-03-30 15:55:10 +0800

    1111

--- SmartCode/SmartCode/MyTreeView.xaml.cs
+++ SmartCode/SmartCode/MyTreeView.xaml.cs
@@ -14,7 +14,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using SmartSQL.Models;
 
-namespace SmartCode
+namespace SmartSQL
 {
     /// <summary>
     /// MyTreeView.xaml 的交互逻辑
