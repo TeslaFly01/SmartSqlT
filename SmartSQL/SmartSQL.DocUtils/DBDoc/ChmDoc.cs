74c6781135f0ea4333a96ea1caf19d7793a801e4
zfluok <zfluok@isoftstone.com>
2022-07-22 17:25:03 +0800

    fix #优化CHM生成不用提前下载安装htmlhelp.exe文件

--- SmartSQL/SmartSQL.DocUtils/DBDoc/ChmDoc.cs
+++ SmartSQL/SmartSQL.DocUtils/DBDoc/ChmDoc.cs
@@ -17,28 +17,27 @@ namespace SmartSQL.DocUtils.DBDoc
 {
     public class ChmDoc : Doc
     {
-        private static string ChmPath = Path.Combine(TplPath, "chm");
+        #region MyRegion
+        /// <summary>
+        /// C:\Users\用户名\AppData\Roaming\SmartSQL\TplFile\chm
+        /// </summary>
+        private static string CHMPath = Path.Combine(TplPath, "chm");
+        /// <summary>
+        /// hhc.exe文件路径
+        /// </summary>
+        private string HHCPath => Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "hhc.exe");
+        /// <summary>
+        /// GBK编码
+        /// </summary>
+        private Encoding Gbk => Encoding.GetEncoding("GB18030");
+
+        private Encoding Utf8 => Encoding.UTF8; 
+        #endregion
+
+
         public ChmDoc(DBDto dto, string filter = "chm files (*.chm)|*.chm") : base(dto, filter)
         {
 
-        }
-
-        private Encoding Gbk => Encoding.GetEncoding("GB18030");
-
-        private Encoding Utf8 => Encoding.UTF8;
-
-        private string HHCPath
-        {
-            get
-            {
-                var hhcPath = string.Empty;
-                var hhwDir = ConfigUtils.SearchInstallDir("HTML Help Workshop", "hhw.exe");
-                if (!string.IsNullOrWhiteSpace(hhwDir) && ZlpIOHelper.DirectoryExists(hhwDir))
-                {
-                    hhcPath = Path.Combine(hhwDir, "hhc.exe");
-                }
-                return hhcPath;
-            }
         }
 
         public override bool Build(string filePath)
@@ -54,54 +53,6 @@ namespace SmartSQL.DocUtils.DBDoc
         private bool BuildDoc(string filePath)
         {
             #region MyRegion
-            #region 使用 HTML Help Workshop 的 hhc.exe 编译 ,先判断系统中是否已经安装有  HTML Help Workshop 
-
-            if (this.HHCPath.IsNullOrWhiteSpace() || !File.Exists(HHCPath))
-            {
-                string htmlhelpPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "htmlhelp.exe");
-
-                if (File.Exists(htmlhelpPath))
-                {
-                    string htmlHelpPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "htmlhelp.exe");
-                    if (File.Exists(htmlHelpPath))
-                    {
-                        Growl.AskGlobal("导出CHM文档需安装 HTML Help Workshop ，是否立即安装？", isConfirm =>
-                        {
-                            if (isConfirm)
-                            {
-                                try
-                                {
-                                    var proc = Process.Start(htmlHelpPath);
-                                }
-                                catch { }
-                            }
-                            return true;
-                        });
-                    }
-                }
-                else
-                {
-                    Growl.AskGlobal("导出CHM文档需安装 HTML Help Workshop ，是否立即下载安装？", isConfirm =>
-                    {
-                        if (isConfirm)
-                        {
-                            try
-                            {
-                                var myProcess = new Process();
-                                //"firefox.exe";// "iexplore.exe";  //chrome  //iexplore.exe //哪个浏览器打开
-                                myProcess.StartInfo.FileName = "chrome.exe";
-                                myProcess.StartInfo.Arguments = "https://gitee.com/izhaofu/SmartSQL/attach_files/1124263/download";
-                                myProcess.Start();
-                            }
-                            catch { }
-                        }
-                        return true;
-                    });
-                }
-                return false;
-            }
-            #endregion
-
             this.InitDirFiles();
 
             var hhc_tpl = Encoding.UTF8.GetString(Resources.hhc);
@@ -123,23 +74,24 @@ namespace SmartSQL.DocUtils.DBDoc
 
             var list = list_tpl.RazorRender(this.Dto);
 
-            ZlpIOHelper.WriteAllText(Path.Combine(this.WorkTmpDir, "chm.hhc"), hhc,Gbk);
+            ZlpIOHelper.WriteAllText(Path.Combine(this.WorkTmpDir, "chm.hhc"), hhc, Gbk);
 
             ZlpIOHelper.WriteAllText(Path.Combine(this.WorkTmpDir, "chm.hhk"), hhk, Gbk);
 
             ZlpIOHelper.WriteAllText(Path.Combine(this.WorkTmpDir, "数据库目录.html"), list, Utf8);
 
+            //生成表
             foreach (var tab in this.Dto.Tables)
             {
-                var tab_path = Path.Combine(this.WorkTmpDir, "table", $"{tab.TableName} {tab.Comment}.html");
+                var tablePath = Path.Combine(this.WorkTmpDir, "table", $"{tab.TableName} {tab.Comment}.html");
                 var content = table_tpl.RazorRender(tab);
-                ZlpIOHelper.WriteAllText(tab_path, content, Utf8);
+                ZlpIOHelper.WriteAllText(tablePath, content, Utf8);
             }
 
-
+            //生成视图
             foreach (var item in Dto.Views)
             {
-                var vw_path = Path.Combine(this.WorkTmpDir, "view", $"{item.ObjectName}.html");
+                var viewPath = Path.Combine(this.WorkTmpDir, "view", $"{item.ObjectName}.html");
                 var content = sqlcode_tpl.RazorRender(
                      new SqlCode()
                      {
@@ -148,22 +100,22 @@ namespace SmartSQL.DocUtils.DBDoc
                          Content = item.Script.Trim()
                      }
                      );
-                ZlpIOHelper.WriteAllText(vw_path, content, Utf8);
+                ZlpIOHelper.WriteAllText(viewPath, content, Utf8);
             }
 
-
+            //生成存储过程
             foreach (var item in Dto.Procs)
             {
-                var proc_path = Path.Combine(this.WorkTmpDir, "proc", $"{item.ObjectName}.html");
+                var procPath = Path.Combine(this.WorkTmpDir, "proc", $"{item.ObjectName}.html");
                 var content = sqlcode_tpl.RazorRender(
-                    new SqlCode()
+                    new SqlCode
                     {
                         DBType = Dto.DBType,
                         CodeName = item.ObjectName,
                         Content = item.Script.Trim()
                     }
                     );
-                ZlpIOHelper.WriteAllText(proc_path, content, Utf8);
+                ZlpIOHelper.WriteAllText(procPath, content, Utf8);
             }
 
             var hhp_Path = Path.Combine(this.WorkTmpDir, "chm.hhp");
@@ -171,9 +123,9 @@ namespace SmartSQL.DocUtils.DBDoc
 
             ZlpIOHelper.WriteAllText(hhp_Path, hhp, Gbk);
 
-            var res = StartRun(HHCPath, hhp_Path, Encoding.GetEncoding("gbk"));
-
-            //LogManager.Info("生成chm信息", res);
+            //开始生成CHM文件
+            StartRun(HHCPath, hhp_Path, Encoding.GetEncoding("gbk"));
+            
             return true;
             #endregion
         }
@@ -202,63 +154,33 @@ namespace SmartSQL.DocUtils.DBDoc
                 ZlpIOHelper.CreateDirectory(tmpDir);
             }
 
-            if (!Directory.Exists(ChmPath))
+            if (!Directory.Exists(CHMPath))
             {
-                Directory.CreateDirectory(ChmPath);
+                Directory.CreateDirectory(CHMPath);
             }
-            if (!Directory.Exists(Path.Combine(ChmPath, "embed")))
+            if (!Directory.Exists(Path.Combine(CHMPath, "embed")))
             {
-                Directory.CreateDirectory(Path.Combine(ChmPath, "embed"));
+                Directory.CreateDirectory(Path.Combine(CHMPath, "embed"));
             }
-            if (!Directory.Exists(Path.Combine(ChmPath, "js")))
+            if (!Directory.Exists(Path.Combine(CHMPath, "js")))
             {
-                Directory.CreateDirectory(Path.Combine(ChmPath, "js"));
+                Directory.CreateDirectory(Path.Combine(CHMPath, "js"));
             }
-            var highlight = Path.Combine(ChmPath, "embed", "highlight.js");
+            var highlight = Path.Combine(CHMPath, "embed", "highlight.js");
             if (!File.Exists(highlight))
             {
                 File.AppendAllText(highlight, Resources.highlight);
             }
-            var sql_formatter = Path.Combine(ChmPath, "embed", "sql-formatter.js");
+            var sql_formatter = Path.Combine(CHMPath, "embed", "sql-formatter.js");
             if (!File.Exists(sql_formatter))
             {
                 File.AppendAllText(sql_formatter, Resources.sql_formatter);
             }
-            var jQuery = Path.Combine(ChmPath, "js", "jQuery.js");
+            var jQuery = Path.Combine(CHMPath, "js", "jQuery.js");
             if (!File.Exists(jQuery))
             {
                 File.AppendAllText(jQuery, Resources.jQuery);
             }
-            //var hhc = Path.Combine(ChmPath, "hhc.cshtml");
-            //var hhk = Path.Combine(ChmPath, "hhk.cshtml");
-            //var hhp = Path.Combine(ChmPath, "hhp.cshtml");
-            //var list = Path.Combine(ChmPath, "list.cshtml");
-            //var sqlcode = Path.Combine(ChmPath, "sqlcode.cshtml");
-            //var table = Path.Combine(ChmPath, "table.cshtml");
-            //if (!File.Exists(hhc))
-            //{
-            //    File.WriteAllBytes(hhc, Resources.hhc);
-            //}
-            //if (!File.Exists(hhk))
-            //{
-            //    File.WriteAllBytes(hhk, Resources.hhk);
-            //}
-            //if (!File.Exists(hhp))
-            //{
-            //    File.WriteAllBytes(hhp, Resources.hhp);
-            //}
-            //if (!File.Exists(list))
-            //{
-            //    File.WriteAllBytes(list, Resources.list);
-            //}
-            //if (!File.Exists(sqlcode))
-            //{
-            //    File.WriteAllBytes(sqlcode, Resources.sqlcode);
-            //}
-            //if (!File.Exists(table))
-            //{
-            //    File.WriteAllBytes(table, Resources.table);
-            //}
             var dir = Path.Combine(TplPath, "chm\\");
 
             var files = Directory.GetFiles(dir, "*.js", SearchOption.AllDirectories);
@@ -273,8 +195,8 @@ namespace SmartSQL.DocUtils.DBDoc
 
         private string StartRun(string hhcPath, string arguments, Encoding encoding)
         {
-            string str = "";
-            ProcessStartInfo startInfo = new ProcessStartInfo()
+            string resultLog = string.Empty;
+            var startInfo = new ProcessStartInfo
             {
                 FileName = hhcPath,  //调入HHC.EXE文件 
                 Arguments = arguments,
@@ -286,16 +208,15 @@ namespace SmartSQL.DocUtils.DBDoc
                 StandardErrorEncoding = encoding,
                 StandardOutputEncoding = encoding
             };
-
-            using (Process process = Process.Start(startInfo))
+            using (var process = Process.Start(startInfo))
             {
-                using (StreamReader reader = process.StandardOutput)
+                using (var reader = process.StandardOutput)
                 {
-                    str = reader.ReadToEnd();
+                    resultLog = reader.ReadToEnd();
                 }
                 process.WaitForExit();
             }
-            return str.Trim();
+            return resultLog.Trim();
         }
 
     }
